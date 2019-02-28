3db12952431918db287ba4d9dba3a04e6463cb82
Paweł Turkot <p.turkot123@gmail.com>
2019-02-28 01:35:47 +0100

    Add section -- kontakt

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,11 @@ document.querySelector('.o-mnie-text').addEventListener('click', () => {
 document.querySelector('.o-mnie-close').addEventListener('click', () => {
     body.classList.remove('o-mnie-on');
 })
+
+document.querySelector('.contact-text').addEventListener('click', () => {
+    body.classList.add('contact-on');
+})
+
+document.querySelector('.contact-close').addEventListener('click', () => {
+    body.classList.remove('contact-on');
+})
