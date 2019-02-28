78b8c719b3c698ea274841e6390fff566c5c545a
Paweł Turkot <p.turkot123@gmail.com>
2019-02-28 01:41:35 +0100

    Add section -- gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,11 @@ document.querySelector('.contact-text').addEventListener('click', () => {
 document.querySelector('.contact-close').addEventListener('click', () => {
     body.classList.remove('contact-on');
 })
+
+document.querySelector('.gallery').addEventListener('click', () => {
+    body.classList.add('gallery-on');
+})
+
+document.querySelector('.gallery-close').addEventListener('click', () => {
+    body.classList.remove('gallery-on');
+})
